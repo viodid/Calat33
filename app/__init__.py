f56a8c64f8a8adca015173ceeeab28db355e5411
viodid <david.zwsse@simplelogin.co>
2022-12-21 16:53:10 +0100

    change padding footer, start artist

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 
-
-app = Flask(__name__) 
+app = Flask(__name__)
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/artista")
+def route():
+    return render_template("artista.html")
