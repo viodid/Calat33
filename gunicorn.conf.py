07b9e9295e7698b3e5ec168751fea71b3501ddb8
viodid <david.zwsse@simplelogin.co>
2023-07-16 20:25:59 +0200

    SEO, update nginx conf

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 
-workers = multiprocessing.cpu_count() * 2 + 1                                                                                                                                                                  
+workers = multiprocessing.cpu_count() * 2 + 1
 
 wsgi_app = 'run:app'
 
